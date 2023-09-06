c6f8be5ef68deff766e88569979d18ac2ca35e11
lailaeliotti1 <93938274+lailaeliotti1@users.noreply.github.com>
2023-09-06 14:26:55 -0500

    Inital commit LE

---
+++ ScoreboardApp/ScoreboardApp/ScoreboardAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScoreboardAppApp.swift
+//  ScoreboardApp
+//
+//  Created by Laila Eliotti on 9/6/23.
+//
+
+import SwiftUI
+
+@main
+struct ScoreboardAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
