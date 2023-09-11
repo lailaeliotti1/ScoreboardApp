1c8d2b75ec3f0dd638e96a6112a12a301e1da00a
lailaeliotti1 <93938274+lailaeliotti1@users.noreply.github.com>
2023-09-11 09:25:32 -0500

    Added swift files-LE

--- ScoreboardApp/ScoreboardApp/ContentView.swift
+++ ScoreboardApp/ScoreboardApp/ContentView.swift
@@ -8,15 +8,86 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var isClockRunning = false
+    @State private var timeRemaining = 600 //ten minutes in sec
+    private var timer: Timer?
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        ZStack {
+            Color(.black).edgesIgnoringSafeArea(.all)
+            VStack{
+                Text("Scoreboard")
+                    .foregroundColor(.gray)
+                    .font(.largeTitle)
+                    .padding()
+                topScoreboard()
+                displayClock()
+            }
         }
         .padding()
     }
+    
+    func topScoreboard() -> some View {
+        return ZStack{
+            Rectangle()
+                .stroke(.blue, lineWidth: 2)
+                .frame(width: 400, height: 300)
+            HStack{
+                Text("Home")
+                    .fontWeight(.semibold)
+                    .foregroundColor(.white)
+                    .padding([.bottom, .trailing], 225)
+                
+                Button(action: {
+                    if isClockRunning {
+                        isClockRunning = false
+                    } else {
+                        isClockRunning = true
+                        startClock()
+                    }
+                
+                }) {
+                    Text(isClockRunning ? "Game ON" : "Timeout")
+                }
+                
+                
+                Text("Away")
+                    .fontWeight(.semibold)
+                    .foregroundColor(.white)
+                    .padding(.leading, 25)
+                    .padding(.bottom, 225)
+            }
+        }
+    }
+    
+    func displayClock() -> some View {
+           return Text("\(timeRemaining / 60):\(timeRemaining % 60)")
+               .font(.largeTitle)
+       }
+    
+    mutating func toggleTimer() {
+           if isClockRunning {
+               stopClock()
+           } else {
+               startClock()
+           }
+           isClockRunning.toggle()
+       }
+    
+    func startClock() {
+        Timer.scheduledTimer(withTimeInterval: 1, repeats: true) { timer in
+            if self.timeRemaining > 0 {
+                self.timeRemaining -= 1
+            } else {
+                timer.stopClock();
+            }
+        }
+    }
+    
+    mutating func stopClock() {
+        timer?.invalidate()
+        timer = nil
+    }
+    
 }
 
 struct ContentView_Previews: PreviewProvider {
